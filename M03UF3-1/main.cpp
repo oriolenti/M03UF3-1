d96176dc1d72d26d6803828c1bb02ebcd804f7fb
DESKTOP-614UO46\Oriol <oriol.valls@enti.cat>
2023-02-18 00:36:52 +0100

    menu y correcciones de map, falta numero de filas

--- M03UF3-1/main.cpp
+++ M03UF3-1/main.cpp
@@ -7,10 +7,5 @@ int main() {
 	tTablero tablero;
 	int fila = 0, columna = 0;
 
-	incicializarTablero(tablero);
-	mostrarTablero(tablero);
-	askPosition(tablero, fila, columna);
-	rellenarCasilla(tablero, fila, columna, X);
-	mostrarTablero(tablero);
 }
 
